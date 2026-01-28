47d76fd892e42fb0f9ef9496a62e6344df83b986
Fahrudin Majetic <mf5793@mci4me.at>
2026-01-28 13:52:12 +0100

    delete double definition

--- lib/Resources/API.dart
+++ lib/Resources/API.dart
@@ -11,5 +11,4 @@ class API {
   static const String getRooms = '/rooms';
   static const String adminRooms = '/admin/rooms';
   static const String getRoomEquipment = '/rooms/equipment';
-  static const String adminRooms = '/admin/rooms';
 }
