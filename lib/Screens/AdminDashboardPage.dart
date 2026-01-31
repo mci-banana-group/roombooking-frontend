6763901efe294b93d0140b5c09e471e3c222b4a2
gernot <gernot.luidolt@gmail.com>
2026-01-31 13:08:51 +0100

    color

--- lib/Screens/AdminDashboardPage.dart
+++ lib/Screens/AdminDashboardPage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'AdminStatsView.dart';
 import 'AdminRoomManagement.dart';
+import '../Resources/AppColors.dart';
 
 class AdminDashboardPage extends ConsumerWidget {
   const AdminDashboardPage({super.key});
@@ -10,22 +11,31 @@ class AdminDashboardPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return DefaultTabController(
       length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text("Admin Dashboard"),
-          bottom: const TabBar(
-            tabs: [
-              Tab(icon: Icon(Icons.meeting_room), text: 'Buildings/Rooms'),
-              Tab(icon: Icon(Icons.bar_chart), text: 'Statistics')
-            ],
+      child: Column(
+        children: [
+          SafeArea(
+            bottom: false,
+            child: Material(
+              child: const TabBar(
+                labelColor: AppColors.mciOrange,
+                unselectedLabelColor: Colors.grey,
+                indicatorColor: AppColors.mciOrange,
+                tabs: [
+                  Tab(icon: Icon(Icons.meeting_room), text: 'Buildings/Rooms'),
+                  Tab(icon: Icon(Icons.bar_chart), text: 'Statistics')
+                ],
+              ),
+            ),
           ),
-        ),
-        body: const TabBarView(
-          children: [
-            AdminRoomManagement(),
-            AdminStatsView(),
-          ],
-        ),
+          const Expanded(
+            child: TabBarView(
+              children: [
+                AdminRoomManagement(),
+                AdminStatsView(),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
